c0f1061a2dd5781ebb5ca685f1514553580ca6d7
Shawn Hartsock <hartsock@acm.org>
2010-03-26 19:18:21 +0000

    mark next release

--- QrcodeGrailsPlugin.groovy
+++ QrcodeGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 class QrcodeGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.1 > *"
     // the other plugins this plugin depends on
