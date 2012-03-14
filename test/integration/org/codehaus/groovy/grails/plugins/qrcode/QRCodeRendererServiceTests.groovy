f29e1e2ca8d2acb1c2809ce392b6718cc3142ea5
Shawn Hartsock <hartsock@acm.org>
2012-03-14 17:18:21 -0400

    simple tests for qrcode service
    Simple tests for QRCode service include verification of binary output.

---
+++ test/integration/org/codehaus/groovy/grails/plugins/qrcode/QRCodeRendererServiceTests.groovy
@@ -0,0 +1,25 @@
+package org.codehaus.groovy.grails.plugins.qrcode
+
+import grails.test.GrailsUnitTestCase
+import org.springframework.core.io.ClassPathResource
+import org.apache.commons.io.IOUtils
+
+class QrCodeRendererServiceTests extends GrailsUnitTestCase {
+    QrCodeRendererService qrCodeRendererService
+    
+    void testSimpleRender() {
+        // a 30 by 30 png image of the word "test" in a QRCode
+        InputStream pngIn =
+            new ClassPathResource("org/codehaus/groovy/grails/plugins/qrcode/qrcode_test.png").getInputStream()
+        byte[] png = IOUtils.toByteArray(pngIn)
+
+        assert pngIn != null
+        assert qrCodeRendererService != null
+
+        OutputStream outputStream = new ByteArrayOutputStream()
+        qrCodeRendererService.renderPng("test",30,outputStream)
+
+        byte[] out = outputStream.toByteArray()
+        assert Arrays.equals(png,out)
+    }
+}
