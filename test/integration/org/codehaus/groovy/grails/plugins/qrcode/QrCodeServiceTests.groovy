a73a51ce10f90d9d215adc5dbe93a8147fdd2000
Shawn Hartsock <hartsock@acm.org>
2012-03-14 21:34:52 -0400

    Remove the code I used to generate a byte array to examine in my hexeditor

--- test/integration/org/codehaus/groovy/grails/plugins/qrcode/QrCodeServiceTests.groovy
+++ test/integration/org/codehaus/groovy/grails/plugins/qrcode/QrCodeServiceTests.groovy
@@ -23,13 +23,4 @@ class QrCodeServiceTests extends GrailsUnitTestCase {
         assert Arrays.equals(png,out)
     }
     
-    void testGenerate() {
-        // a 30 by 30 png image of the word "test" in a QRCode
-        File file =
-            new File("/Users/hartsock/Desktop/qrcode.png")
-        OutputStream outputStream = file.newOutputStream()
-        qrCodeService.renderPng("test",30,outputStream)
-	outputStream.flush()
-	outputStream.close()
-    }
 }
