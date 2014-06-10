f03d5e8f0a2d76b8582d8127be5094af5b6d0f33
Goran Ehrsson <goran@technipelago.se>
2014-06-10 22:13:14 +0200

    Upgraded zxing to 3.1.0 and pngj to 2.1.0. All dependencies in BuildConfig, no more jars in lib

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,37 +1,24 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global") { }
+    log "warn"
     repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
         mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-        compile "com.google.zxing:core:2.0"
+        compile "ar.com.hjg:pngj:2.1.0"
+        compile "com.google.zxing:core:3.1.0"
     }
-
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.0.3") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
         runtime(":hibernate:$grailsVersion") {
