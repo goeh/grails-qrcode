4eebfcf5d37b9d9163e3f63a6d90b66485c29b04
Burt Beckwith <burt@burtbeckwith.com>
2015-04-02 00:25:31 -0400

    need an explicit non-exported dependency for the rest-client-builder plugin since some versions of Grails have a bug where transitive dependencies of excluded plugins aren't excluded, so they end up installed in the application

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', {
+        build ':release:3.0.1', ':rest-client-builder:2.0.3', {
             export = false
         }
     }
