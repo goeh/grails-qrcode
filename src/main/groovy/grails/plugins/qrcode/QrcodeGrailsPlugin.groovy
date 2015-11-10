1992065360a17497ecbc1163a4ae5fa62fc573a6
Cleydson Júnior <cleydson.fibonacci@solucoesageis.com.br>
2015-11-10 14:08:32 -0200

    grails version migration step1: adding gradle build and moving configuration files

--- src/main/groovy/grails/plugins/qrcode/QrcodeGrailsPlugin.groovy
+++ src/main/groovy/grails/plugins/qrcode/QrcodeGrailsPlugin.groovy
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package grails.plugins.qrcode
+
 class QrcodeGrailsPlugin {
     def version = "0.7"
     def grailsVersion = "2.0.0 > *"
