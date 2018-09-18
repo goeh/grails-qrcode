4a98e54620409c6dc83704a1710859f1f5887f94
Goran Ehrsson <goran@technipelago.se>
2018-09-18 13:15:48 +0200

    Project now uses Grails 2.4.5

--- QrcodeGrailsPlugin.groovy
+++ QrcodeGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 class QrcodeGrailsPlugin {
-    def version = "0.7"
+    def version = "0.7.1"
     def grailsVersion = "2.0.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
